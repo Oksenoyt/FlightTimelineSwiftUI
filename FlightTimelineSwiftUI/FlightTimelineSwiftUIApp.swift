19cb1de0f0c21405e177552044f304ae810c65df
Oksenoyt <a.oksenoyt@gmail.com>
2023-06-16 10:51:29 -0700

    Initial Commit

---
+++ FlightTimelineSwiftUI/FlightTimelineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimelineSwiftUIApp.swift
+//  FlightTimelineSwiftUI
+//
+//  Created by Elenka on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimelineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
